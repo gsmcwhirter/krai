cedae362a79cf4955b0647563fd644b88f1c4ae1
Gregory McWhirter <greg@hallofkvasir.org>
2009-04-26 15:05:26 -0700

    Adding the request exception class

---
+++ Krai/Request/Exception.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * A Krai exception class
+ * @package Krai
+ * @subpackage Request
+ * @author Greg McWhirter <gsmcwhirter@gmail.com>
+ * @copyright Copyright (c) 2008, Greg McWhirter
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+/**
+ * Request exception class
+ *
+ * This class is the exception that should be thrown by anything related to Krai_Request.
+ *
+ * @package Krai
+ * @subpackage Request
+ */
+class Krai_Request_Exception extends Krai_Exception
+{ }
