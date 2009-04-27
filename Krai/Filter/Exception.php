6ae25336651ad6e6a10ffb014a31d0adb43c9e2a
Gregory McWhirter <greg@hallofkvasir.org>
2009-04-27 10:49:06 -0700

    Update to the generate_phpdoc script to correct a typo, and documenting the filter classes so phpdoc stops complaining.

--- Krai/Filter/Exception.php
+++ Krai/Filter/Exception.php
@@ -1,6 +1,20 @@
 <?php
+/**
+ * A Krai exception class
+ * @package Krai
+ * @subpackage Filter
+ * @author Greg McWhirter <gsmcwhirter@gmail.com>
+ * @copyright Copyright (c) 2008, Greg McWhirter
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ */
 
+/**
+ * Filter exception class
+ *
+ * This class is the exception that should be thrown by anything related to Krai_Filter.
+ *
+ * @package Krai
+ * @subpackage Filter
+ */
 class Krai_Filter_Exception extends Krai_Exception
-{
-
-}
+{}
